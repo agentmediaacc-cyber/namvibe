e5a862636ad037cac0bdb3e2741dafe239937ae8
agentmediaacc-cyber <agentmediaacc@gmail.com>
2026-04-17 21:17:09 +0200

    Fix broken urls.py syntax

--- config/urls.py
+++ config/urls.py
@@ -3,17 +3,18 @@ from django.http import HttpResponse
 from django.urls import path, include
 from core.views import index as real_index
 
+
 def safe_index(request):
     try:
         return real_index(request)
     except Exception as e:
         return HttpResponse(f"Namvibe safe mode<br><br>{e}")
 
+
 urlpatterns = [
     path("", safe_index),
     path("healthz", lambda request: HttpResponse("ok")),
     path("admin/", admin.site.urls),
-
     path("accounts/", include("accounts.urls")),
     path("posts/", include("posts.urls")),
     path("feed/", include("posts.urls")),
