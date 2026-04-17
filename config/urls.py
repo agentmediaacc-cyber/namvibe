9cf64dad16f8b10662bca327181d08c67f17a07a
agentmediaacc-cyber <agentmediaacc@gmail.com>
2026-04-17 21:26:06 +0200

    Add community_list route alias

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
 from core.views import index as real_index
+from django.shortcuts import redirect
+
+def community_list_alias(request):
+    return redirect("/communities/")
 
 
 def safe_index(request):
@@ -15,10 +19,12 @@ urlpatterns = [
     path("", safe_index, name="home"),
     path("healthz", lambda request: HttpResponse("ok")),
     path("admin/", admin.site.urls),
+    path("community-list/", community_list_alias, name="community_list"),
     path("accounts/", include("accounts.urls")),
     path("posts/", include("posts.urls")),
     path("feed/", include("posts.urls")),
     path("live/", include("live.urls")),
     path("wallet/", include("wallet.urls")),
     path("dating/", include("dating.urls")),
+    path("communities/", include(("communities.urls", "communities"), namespace="communities")),
 ]
